654304e562ca3df1aa948500d75cff0493923a56
rngustj9139 <mnb9139@daum.net>
2022-04-10 13:25:20 +0900

    Member repository & Member service

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MemberService {
+
+//    @Autowired
+//    private MemberRepository memberRepository; // 필드 주입
+
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) { // 세터 주입(필드주입과는 다르게 test를 할때 Mock 같은 것을 주입해줄 수 있다는 장점이 존재한다 but 어플리케이션 로딩시간에 값을 변경시킬 수 있어 일관성이 떨어질 수 있다는 문제점이 존재한다.)
+//        this.memberRepository = memberRepository;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) { // 생성자 주입
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입 기능
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) { // 이렇게 해도 동시성 문제가 발생할 수 있음
+        List<Member> findedMembers = memberRepository.findByName(member.getName());
+
+        if (!findedMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회 기능
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 단일 회원 조회 기능
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,34 +1,36 @@
-package jpabook.jpashop;
-
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.*;
-
-@RunWith(SpringRunner.class) // 스프링을 써서 테스트 할거라고 선언
-@SpringBootTest // 스프링 부트를 써서 통합 테스트를 수행
-public class MemberRepositoryTest {
-
-    @Autowired // 의존관계 자동 주입
-    MemberRepository memberRepository;
-
-    @Test
-    @Transactional // jpa는 transaction이라는 단위 속에서 실행되서 이 어노테이션을 붙여야함 하지만 이 어노테이션이 test에 있으면 db에 저장을 해도 롤백 시켜버린다. 롤백하기 싫으면 @Rollback(false) 쓰면된다.
-    @Rollback(false)
-    public void testMember() throws Exception {
-        Member member = new Member();
-        member.setUsername("memberA");
-        Long savedId = memberRepository.save(member);
-
-        Member findedMember = memberRepository.find(savedId);
-
-        Assertions.assertThat(findedMember).isEqualTo(member);
-    }
-
-}
+//package jpabook.jpashop;
+//
+//import jpabook.jpashop.domain.Member;
+//import jpabook.jpashop.repository.MemberRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(SpringRunner.class) // 스프링을 써서 테스트 할거라고 선언
+//@SpringBootTest // 스프링 부트를 써서 통합 테스트를 수행
+//public class MemberRepositoryTest {
+//
+//    @Autowired // 의존관계 자동 주입
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional // jpa는 transaction이라는 단위 속에서 실행되서 이 어노테이션을 붙여야함 하지만 이 어노테이션이 test에 있으면 db에 저장을 해도 롤백 시켜버린다. 롤백하기 싫으면 @Rollback(false) 쓰면된다.
+//    @Rollback(false)
+//    public void testMember() throws Exception {
+//        Member member = new Member();
+//        member.setName("memberA");
+//        Long savedId = memberRepository.save(member);
+//
+//        Member findedMember = memberRepository.find(savedId);
+//
+//        Assertions.assertThat(findedMember).isEqualTo(member);
+//    }
+//
+//}
