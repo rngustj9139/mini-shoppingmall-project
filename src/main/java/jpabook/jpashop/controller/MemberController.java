413cabbce4954fede9bb8fba4803804b97cdd872
rngustj9139 <mnb9139@daum.net>
2022-07-01 11:22:27 +0900

    Add MemberForm, MemberController, createMemberForm.html

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+}
