b42510028f5f50e0d94f452b93fb321455d3f39d
rngustj9139 <mnb9139@daum.net>
2022-07-16 02:56:25 +0900

    Add comment

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -89,7 +89,7 @@ public class OrderApiController { // OrderSimpleApiController는 ManyToOne이나
     }
 
     @GetMapping("/api/v6/orders")
-    public List<OrderQueryDto> orderV6() { // 상황에 따라 orderV6가 orderV5보다 느릴 수 있다.
+    public List<OrderQueryDto> orderV6() { // 상황에 따라 orderV6가 orderV5보다 느릴 수 있다. (쿼리는 한번이지만 데이터 중복이 발생하고 메모리 영역에서 분할을 해주어야한다. - 중복을 없에야한다. 페이징도 불가능)
         List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
 
         return flats.stream() // 데이터 중복 방지, grouping을 사용하려면 OrderQueryDto에 @EqualsAndHashCode 어노테이션을 추가해야함
